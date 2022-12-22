06ae52766e433550cbfa54b1b4a37e02c495a156
OxanaSF <oxanatango@gmail.com>
2022-12-21 16:00:23 -0800

    recreate ReviewView(form) using buil-in FormView

--- reviews/views.py
+++ reviews/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
 from .forms import ReviewForm
 from .models import Review
@@ -10,23 +11,14 @@ from .models import Review
 
 # Create your views here.
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
-
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 
 class ThankYouView(TemplateView):
@@ -45,13 +37,10 @@ class ReviewsListView(ListView):
 
     def get_queryset(self):
         base_query = super().get_queryset()
-        data = base_query.filter(rating__gt=4)
-        return data
+        # data = base_query.filter(rating__gt=4)
+        return base_query
 
 
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
-    
-
- 
